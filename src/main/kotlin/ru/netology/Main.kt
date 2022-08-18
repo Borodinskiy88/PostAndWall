90b68cf01757b12e12524214c8c84daee7945089
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-08-18 18:10:31 +0300

    Работает

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -2,7 +2,7 @@ package ru.netology
 
 fun main() {
     val post = Post(
-        0, 1, 1, 10, "Hello!", " ",
+        0, 1, 1, 10, "Hello!", "Yes",
         likes = Likes(120), comments = Comments(12)
     )
 
@@ -13,17 +13,8 @@ fun main() {
         )
     )
 
-    val post3 = WallService.add(
-        post = Post(
-            0, 45, 3, 50,
-            "Hi", "", likes = Likes(300), comments = Comments(47)
-        )
-    )
-
     println(post)
-//    println(post2)
-//    println(post3)
-    println(WallService.add(post))
+    println(post2)
     println(WallService.update(post))
     println(WallService.add(post))
 
@@ -61,73 +52,34 @@ data class Comments(
     val canOpen: Boolean = false
 )
 
-/*
-Теперь нужно разобраться с функциональностью сервиса,
-отвечающего за стены пользователей: https://vk.com/dev/wall (копия).
-
-Нас будут интересовать следующие методы:
-
-Создание записи.
-Обновление записи.
-
-Создание записи
-Итак, поехали, метод для создания записи должен выглядеть вот так:
-
-    fun add(post: Post): Post {
-        TODO()
-    }
-Как он должен работать:
-
-Он должен добавлять запись в массив,
-но при этом назначать посту уникальный среди всех постов идентификатор.
-Возвращать пост с уже выставленным идентификатором.
-Подсказка
-Возможно, вам стоит завести private переменную для хранения следующего уникального id.
-Пока у нас in-memory система — всё хранится в оперативной памяти,
-и всё работает на одной машине, этого будет достаточно.
-
- */
 object WallService {
     private var posts = emptyArray<Post>()
     private var id = 0
 
 
     fun add(post: Post): Post {
-//        id++
+        id++
         posts += post.copy(id = id)
-        return posts.last()
+        return posts.first()
     }
 
-
-    /*
-    Обновление записи
-    Метод для создания записи должен выглядеть вот так:
-
-        fun update(post: Post): Boolean {
-            TODO()
-        }
-    Как он должен работать:
-
-    Он должен находить среди всех постов запись с тем же id,
-    что и у post и обновлять все свойства, кроме id владельца и даты создания.
-    Если пост с таким id не найден, то ничего не происходит и возвращается false,
-    в противном случае - возвращается true.
-     */
     fun update(post: Post): Boolean {
-        for ((index, value ) in posts.withIndex()) {
+        for ((index, newPost) in posts.withIndex()) {
             if (post.id == id) {
                 posts[index] = post.copy(
                     id = id,
-                    [index] = value.ownerId,
-                    fromId = value.fromId,
+                    ownerId = post.ownerId,
+                    fromId = newPost.fromId,
                     date = post.date,
-                    text = value.text,
-                    copyright = value.copyright,
-                    likes = value.likes,
-                    comments = value.comments
+                    text = newPost.text,
+                    copyright = newPost.copyright,
+                    likes = newPost.likes,
+                    comments = newPost.comments
                 )
 
-            } else { return false }
+            } else {
+                return false
+            }
         }
         return true
     }
