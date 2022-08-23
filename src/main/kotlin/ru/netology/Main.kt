67fe362651d1d428f83510200249bbf1494eb204
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-08-23 12:50:15 +0300

    Test new

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,17 +1,21 @@
 package ru.netology
 
+import java.lang.reflect.Array
+
 fun main() {
     val post = WallService.add(
         post = Post(
-            0, 5, 5, 50, "Hello", "No",
-            likes = Likes(35), comments = Comments(45)
+            0, 5, 5, 50, 12, "No", "No", 5, 5,
+            likes = Likes(35), comments = Comments(45), views = Views(), repost = Repost(),
+            geo = Geo(place = Place()), postSource = PostSource()
         )
     )
 
     val post2 = WallService.update(
         newPost = Post(
-            0, 3, 3, 40, "Hi", "Maybe",
-            likes = Likes(23), comments = Comments(32)
+            0, 3, 3, 40, 23, "Maybe", "Yes", 0, 0,
+            likes = Likes(23), comments = Comments(32), views = Views(), repost = Repost(),
+            geo = Geo(place = Place()), postSource = PostSource()
         )
     )
 
@@ -25,16 +29,31 @@ data class Post(
     var id: Int,
     val ownerId: Int,
     val fromId: Int,
+    val createdBy: Int?,
     val date: Int,
     val text: String,
-    val copyright: String,
+    val copyright: String?,
+    val replyOwnerId: Int?,
+    val replyPostId: Int?,
     val friendsOnly: Boolean = true,
     val canPin: Boolean = true,
     val canDelete: Boolean = true,
     val canEdit: Boolean = true,
     val isFavorite: Boolean = false,
     val likes: Likes,
-    val comments: Comments
+    val repost: Repost,
+    val comments: Comments,
+    val views: Views,
+    val postType: String = "post",
+    val geo: Geo?,
+    val signerId: Int = 0,
+    val copyHistory: Array? = null,
+    val isPinned: Boolean = false,
+    val markedAsAds: Boolean = false,
+    val postponedId: Int = 0,
+    val postSource: PostSource
+
+
 )
 
 data class Likes(
@@ -50,6 +69,32 @@ data class Comments(
     val groupCanPost: Boolean = false,
     val canClose: Boolean = false,
     val canOpen: Boolean = false
+)
+
+data class Repost(
+    val count: Int = 0,
+    val userReposted: Boolean = false,
+)
+
+data class Views(
+    val count: Int? = 0
+)
+
+data class Geo(
+    val type: String? = "city",
+    val coordinates: String? = "0.0",
+    val place: Place
+)
+
+data class Place(
+    val placeView: String? = "Non"
+)
+
+data class PostSource(
+    val type: String = "vk",
+    val platform: String = "android",
+    val data: String = "likes",
+    val url: String? = "www"
 )
 
 object WallService {
