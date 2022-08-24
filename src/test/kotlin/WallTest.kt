fa86329bd30c45335cea67bd1604097ba21f435b
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-08-24 18:11:37 +0300

    Comment

--- src/test/kotlin/WallTest.kt
+++ src/test/kotlin/WallTest.kt
@@ -16,7 +16,7 @@ class WallTest {
         val addPost = WallService.add(
             post = Post(
                 0, 0, 0, 0, 0, "", " ", 0, 0,
-                likes = Likes(0), comments = Comments(0),
+                likes = Likes(0), commentsPost = CommentsPost(0),
                 views = Views(0), repost = Repost(0), geo = Geo(place = Place()),
                 postSource = PostSource()
             )
@@ -24,7 +24,7 @@ class WallTest {
         val addPost2 = WallService.add(
             post = Post(
                 0, 1, 1, 1, 1, "1", "No", 1, 1,
-                likes = Likes(1), comments = Comments(1), views = Views(1), repost = Repost(1),
+                likes = Likes(1), commentsPost = CommentsPost(1), views = Views(1), repost = Repost(1),
                 geo = Geo(place = Place()), postSource = PostSource()
             )
         )
@@ -52,28 +52,28 @@ class WallServiceTest() {
         service.add(
             post = Post(
                 0, 0, 0, 0, 0, "", " ", 1, 1,
-                likes = Likes(0), comments = Comments(0), views = Views(), repost = Repost(),
+                likes = Likes(0), commentsPost = CommentsPost(0), views = Views(), repost = Repost(),
                 geo = Geo(place = Place()), postSource = PostSource()
             )
         )
         service.add(
             post = Post(
                 1, 22, 22, 22, 22, "", " ", 22, 22,
-                likes = Likes(22), comments = Comments(22), views = Views(), repost = Repost(),
+                likes = Likes(22), commentsPost = CommentsPost(22), views = Views(), repost = Repost(),
                 geo = Geo(place = Place()), postSource = PostSource()
             )
         )
         service.add(
             post = Post(
                 2, 33, 33, 33, 33, "", "", 33, 33,
-                likes = Likes(33), comments = Comments(33), views = Views(), repost = Repost(),
+                likes = Likes(33), commentsPost = CommentsPost(33), views = Views(), repost = Repost(),
                 geo = Geo(place = Place()), postSource = PostSource()
             )
         )
 
         val update = Post(
             1, 111, 111, 111, 111, "212", "", 111, 111,
-            likes = Likes(234), comments = Comments(457), views = Views(), repost = Repost(),
+            likes = Likes(234), commentsPost = CommentsPost(457), views = Views(), repost = Repost(),
             geo = Geo(place = Place()), postSource = PostSource()
         )
 
@@ -90,28 +90,28 @@ class WallServiceTest() {
         service.add(
             post = Post(
                 0, 0, 0, 0, 1, "", "", 0, 0,
-                likes = Likes(0), comments = Comments(0), views = Views(), repost = Repost(),
+                likes = Likes(0), commentsPost = CommentsPost(0), views = Views(), repost = Repost(),
                 geo = Geo(place = Place()), postSource = PostSource()
             )
         )
         service.add(
             post = Post(
                 1, 22, 22, 22, 22, "", "", 0, 0,
-                likes = Likes(22), comments = Comments(22), views = Views(), repost = Repost(),
+                likes = Likes(22), commentsPost = CommentsPost(22), views = Views(), repost = Repost(),
                 geo = Geo(place = Place()), postSource = PostSource()
             )
         )
         service.add(
             post = Post(
                 2, 33, 33, 33, 33, "", "", 0, 0,
-                likes = Likes(33), comments = Comments(33), views = Views(), repost = Repost(),
+                likes = Likes(33), commentsPost = CommentsPost(33), views = Views(), repost = Repost(),
                 geo = Geo(place = Place()), postSource = PostSource()
             )
         )
 
         val update = Post(
             55, 0, 0, 0, 0, "", "", 0, 0,
-            likes = Likes(0), comments = Comments(0), views = Views(), repost = Repost(),
+            likes = Likes(0), commentsPost = CommentsPost(0), views = Views(), repost = Repost(),
             geo = Geo(place = Place()), postSource = PostSource()
         )
 
